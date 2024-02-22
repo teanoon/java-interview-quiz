1316600c12004f3061c9667a1cf43d9a96e7f706
teanoon <anticipationvu@gmail.com>
2024-02-22 19:51:35 +0800

    add comment

--- app/src/main/java/org/example/SimpleService.java
+++ app/src/main/java/org/example/SimpleService.java
@@ -5,6 +5,9 @@ public class SimpleService {
     private static final int BIG_BRICK_SIZE = 5;
     private static final int SMALL_BRICK_SIZE = 2;
 
+    /**
+     * given the gap and a few bricks, check if they can fill the gap
+     */
     boolean canFillGap(int smallQuantity, int bigQuantity, int totalGap) {
         int maxBigRequired = totalGap / BIG_BRICK_SIZE;
         if (maxBigRequired > bigQuantity) {
@@ -19,10 +22,16 @@ public class SimpleService {
         return gapLeftMaxBigRequired % BIG_BRICK_SIZE == 0;
     }
 
+    /**
+     * given a string, encode it using length encoding
+     */
     public String lengthEncode(String input) {
         return null;
     }
 
+    /**
+     * given an integer, reverse it
+     */
     public int reverse(int input) {
         return 0;
     }
